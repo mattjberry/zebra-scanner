6c45101f7ce51d1dd0884c458e7d78960822d241
Matt Berry <matthewjberry03@gmail.com>
2026-05-07 10:44:52 -0600

    error page, pretty straightforward

---
+++ frontend/src/components/ErrorPage.jsx
@@ -0,0 +1,12 @@
+export default function ErrorPage({errorMessage, reset}) {
+    return
+        <div className="error-page">
+            <span className="error-page__message mono">
+                Uh oh! {errorMessage}
+            </span>
+
+            <button type="button" onClick={reset} className="error-page__reset-button">
+                Go back
+            </button>
+        </div>
+}
