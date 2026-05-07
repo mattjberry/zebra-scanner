cc9f94b45a5a929363b87d4cca502b0741b28ba8
Matt Berry <matthewjberry03@gmail.com>
2026-05-07 10:44:36 -0600

    comments

--- frontend/src/components/ResultsPage.jsx
+++ frontend/src/components/ResultsPage.jsx
@@ -1,18 +1,23 @@
 export default function ResultsPage({originalImage, upc, product, reset}) {
     return 
         <div className="results-page">
+            {/* display original given image */}
             <div className="results-page__original-image">
                 <span className="results-page__original-label mono">
                     You gave:
                 </span>
                 <img src={originalImage}></img>
             </div>
+
+            {/* display the upc barcode that the image became */}
             <div className="results-page__upc">
                 <span className="results-page__upc-label mono">
                     Which became:
                 </span>
                 <img src={upc}></img>
             </div>
+
+            {/* display resulting product */}
             <div className="results-page__product-area">
                 <span className="results-page__product-title mono">
                     {product.product}
@@ -21,6 +26,7 @@ export default function ResultsPage({originalImage, upc, product, reset}) {
                 {/* go to first offer link found from look up */}
                 <button type="button" onClick={product.offers[1]}>Buy Now!</button>
 
+                {/* reset state and go back to the upload page */}
                 <button type="button" onClick={reset}>Scan Again!</button>
             </div>
         </div>
